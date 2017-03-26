9435dea502761d6ccdc6ebb7662ad3761284f570
jw1 <warlick.james@gmail.com>
2017-03-26 18:07:36 -0500

    Start working on logins

--- insmart/urls.py
+++ insmart/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+
+    # add real stuff here
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+
+    # tutorial stuff
     url(r'^polls/', include('polls.urls')),
+
+    # administration
     url(r'^admin/', admin.site.urls),
 ]
