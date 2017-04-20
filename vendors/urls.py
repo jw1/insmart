551f2668f94edbe9866e358f6c82361b6ab27936
loukleon2021 <loukleon2021@gmail.com>
2017-04-19 23:38:18 -0500

    created CRUD for Vendors

---
+++ vendors/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from vendors import views
+
+urlpatterns = [
+  url(r'^$', views.vendor_list, name='vendor_list'),
+  url(r'^new$', views.vendor_create, name='vendor_new'),
+  url(r'^edit/(?P<pk>\d+)$', views.vendor_update, name='vendor_edit'),
+  url(r'^delete/(?P<pk>\d+)$', views.vendor_delete, name='vendor_delete'),
+]
