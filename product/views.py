3b33ea1718cb003591fcd71e48905f5e910368cd
LeonelC <leonelcabrerajr@gmail.com>
2017-04-20 15:33:31 -0500

    Product UpdateView current field is now read-only Minor text fixes to product templates.

--- product/views.py
+++ product/views.py
@@ -1,13 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from django.shortcuts import render, redirect, get_object_or_404
 from product.models import Product
 from django.views.generic import DetailView
 
-class ProductForm(ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['vendors', 'name', 'description', 'brand',
               'minimum', 'maximum', 'current', 'active']
+
+class UpdateProductForm(ProductForm):
+    current = forms.CharField(disabled=True)
 
 class ProductDetail(DetailView):
     queryset = Product.objects.all()
@@ -31,7 +34,7 @@ def product_create(request, template_name = 'product/product_form.html'):
 
 def product_update(request, pk, template_name='product/product_form.html'):
     product = get_object_or_404(Product, pk=pk)
-    form = ProductForm(request.POST or None, instance = product)
+    form = UpdateProductForm(request.POST or None, instance = product)
     if form.is_valid():
         form.save()
         return redirect('product_list')
