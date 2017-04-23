83b4a13573218661dab1680514f95d5810b56fc1
James Warlick <jwarlick@financialpayments.com>
2017-04-23 13:25:53 -0500

    make product updates potentially generate alerts

--- product/views.py
+++ product/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from product.models import Product
 from django.views.generic import DetailView
 from insmart_core.search import get_query
+from inventory.models import AuditLog
+from alert.models import Alert
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -41,17 +43,60 @@ def product_create(request, template_name = 'product/product_form.html'):
         return redirect('product_list')
     return render(request, template_name, {'form':form})
 
-def product_update(request, pk, template_name='product/product_form.html'):
-    product = get_object_or_404(Product, pk=pk)
-    form = UpdateProductForm(request.POST or None, instance = product)
-    if form.is_valid():
-        form.save()
-        return redirect('product_list')
-    return render(request, template_name, {'form':form})
-
 def product_delete(request, pk, template_name='product/product_confirm_delete.html'):
     product = get_object_or_404(Product, pk=pk)
     if request.method=='POST':
         product.delete()
         return redirect('product_list')
     return render(request, template_name, {'object':product})
+
+def product_update(request, pk, template_name='product/product_form.html'):
+    product = get_object_or_404(Product, pk=pk)
+    form = UpdateProductForm(request.POST or None, instance = product)
+    if form.is_valid():
+
+        # Note the "pre-save" values so we can see if we generated an alert
+        before_save_minimum = form.initial['minimum']
+        before_save_maximum = form.initial['maximum']
+
+        form.save()
+
+        # TODO:  Need to check here and see if the updated min / max settings will generate an alert.
+        #        But only if we were previously not in "alert territory"
+        if (is_alert_needed(product, before_save_minimum, before_save_maximum)):
+            generate_alert(product, request.user)
+
+        return redirect('product_list')
+    return render(request, template_name, {'form':form})
+
+def is_alert_needed(product, before_save_minimum, before_save_maximum):
+
+    # before it was okay, after update we're under the limit
+    if before_save_minimum <= product.current < product.minimum:
+        return True
+
+    # before it was okay, after update we're over the limit
+    if before_save_maximum >= product.current > product.maximum:
+        return True
+
+    return False
+
+def generate_alert(product, user):
+    # build an audit record to represent the product change
+    audit = AuditLog()
+    audit.product = product
+    audit.user_id = user
+    audit.before = product.current
+    audit.after = product.current
+    audit.adjustment = 0
+    audit.memo = 'Auto-generated inventory adjustment -- product minimum and maximum changed.'
+    audit.save()
+
+    # and generate an alert associated with that audit record
+    alert = Alert()
+    alert.product = product
+    alert.audit_log = audit
+    alert.minimum = product.minimum
+    alert.maximum = product.maximum
+    alert.current = product.current
+    alert.save()
