ff0e2947ee3465b941871daac65f8b08129f4e0f
James Warlick <jwarlick@financialpayments.com>
2017-04-26 14:48:25 -0500

    no debug, no problems

--- insmart/settings.py
+++ insmart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '21(3m0lphxuf=r@onw4#$-6kvf)z(8ylrk4i5yy(5uv7r%#x$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'secure-atoll-90896.herokuapp.com',
